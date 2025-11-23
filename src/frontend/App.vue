548d8cc48b1aee93518b069d369ef48d34ef9ab4
EdisonnTran <tran.edison52@gmail.com>
2025-11-23 12:22:38 -0500

    moved main.js back

--- src/frontend/App.vue
+++ src/frontend/App.vue
@@ -1,12 +1,16 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Hello',
+    };
+  },
+};
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <h1>{{ name }}</h1>
 </template>
 
 <style scoped></style>
